88bc6978c1aa0a7da3dae6aa21c22d237aa881db
damon-sava-stanley <damonsava@gmail.com>
2017-09-02 13:04:21 -0400

    Put build script in loop
    Could not figure out filewatcher quickly enough to stomach so went with the simplest thing I could think of.

--- compile.ps1
+++ compile.ps1
@@ -1,12 +1,15 @@
-Get-ChildItem "blogs/" -Filter *.md | Foreach-Object {
-  $bib="blogs/$_" -replace ".md", ".bib"
-  $html="blogs/html/$_" -replace ".md", ".html"
-  $pdf="blogs/html/$_" -replace ".md", ".pdf"
-  ./gpp.exe -H -DHTML=1 -o tmp.md blogs/$_
-  pandoc tmp.md --template=template.html --toc --bibliography=$bib -o $html
-  rm tmp.md
-  ./gpp.exe -H -DTEX=1 -o tmp.md blogs/$_
-  pandoc tmp.md --toc --bibliography=$bib -o $pdf
-  rm tmp.md
+while ($true) {
+  Get-ChildItem "blogs/" -Filter *.md | Foreach-Object {
+    $bib="blogs/$_" -replace ".md", ".bib"
+    $html="blogs/html/$_" -replace ".md", ".html"
+    $pdf="blogs/html/$_" -replace ".md", ".pdf"
+    ./gpp.exe -H -DHTML=1 -o tmp.md blogs/$_
+    pandoc tmp.md --template=template.html --toc --bibliography=$bib -o $html
+    rm tmp.md
+    ./gpp.exe -H -DTEX=1 -o tmp.md blogs/$_
+    pandoc tmp.md --toc --bibliography=$bib -o $pdf
+    rm tmp.md
+  }
+  pandoc blog.md --template=template.html --toc -o blog.html
+  sleep 30
 }
-pandoc blog.md --template=template.html --toc -o blog.html
