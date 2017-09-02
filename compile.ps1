c0150d767b4d272e0b6b54acca2d27edea7dae17
damon-sava-stanley <damonsava@gmail.com>
2017-09-01 20:07:24 -0400

    Introduced pdf rendering
    Also introduced gpp as a nice macro system to achieve better cross-format results.

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,12 @@
 Get-ChildItem "blogs/" -Filter *.md | Foreach-Object {
-  $output="blogs/html/$_" -replace ".md", ".html"
-  pandoc blogs/$_ --template=template.html --toc -o $output
+  $bib="blogs/$_" -replace ".md", ".bib"
+  $html="blogs/html/$_" -replace ".md", ".html"
+  $pdf="blogs/html/$_" -replace ".md", ".pdf"
+  ./gpp.exe -H -DHTML=1 -o tmp.md blogs/$_
+  pandoc tmp.md --template=template.html --toc --bibliography=$bib -o $html
+  rm tmp.md
+  ./gpp.exe -H -DTEX=1 -o tmp.md blogs/$_
+  pandoc tmp.md --toc --bibliography=$bib -o $pdf
+  rm tmp.md
 }
 pandoc blog.md --template=template.html --toc -o blog.html
