0fc5e046e731e2800254eb41de866fbd5c93166b
Damon Stanley <damonsava@gmail.com>
2018-03-14 09:10:32 -0400

    IMP: Changed preprocessor to PP
    C.f. http://cdsoft.fr/pp/ This is usefully simpler than gpp, has some
    extra potentially useful features and is easier to access.

--- compile.ps1
+++ compile.ps1
@@ -5,13 +5,9 @@ while ($true) {
     $bib="blogs/$_" -replace ".md", ".bib"
     $html="blogs/html/$_" -replace ".md", ".html"
     $pdf="blogs/html/$_" -replace ".md", ".pdf"
-    ./gpp.exe -H -DHTML=1 -o tmp.md blogs/$_
-    pandoc tmp.md --template=template.html --toc --bibliography=$bib -o $html
-    rm tmp.md
+    ./pp.exe -en -html "blogs/$_" | pandoc --template=template.html --toc --bibliography="$bib" -o "$html"
     if ($tex) {
-      ./gpp.exe -H -DTEX=1 -o tmp.md blogs/$_
-      pandoc tmp.md --toc --bibliography=$bib -o $pdf
-      rm tmp.md
+      ./pp.exe -en -pdf "blogs/$_" | pandoc --toc --bibliography="$bib" -o "$pdf"
     }
   }
 
@@ -19,13 +15,10 @@ while ($true) {
     $bib="slides/$_" -replace ".md", ".bib"
     $html="slides/html/$_" -replace ".md", ".html"
     $pdf="slides/html/$_" -replace ".md", ".pdf"
-    ./gpp.exe -H -DHTML=1 -o tmp.md slides/$_
-    pandoc -t revealjs -s -V revealjs-url=reveal-js tmp.md  --bibliography=$bib -o $html --include-in-header=reveal.css.html
+    ./pp.exe -en -html slides/$_ | pandoc -t revealjs -s -V revealjs-url=reveal --bibliography="$bib" -o "$html" --include-in-header=reveal.css.html
     rm tmp.md
     if ($tex) {
-      ./gpp.exe -H -DTEX=1 -o tmp.md slides/$_
-     pandoc -t beamer tmp.md --bibliography=$bib -o $pdf
-     rm tmp.md
+      ./pp.exe -en -pdf slides/$_ | pandoc -t tmp.md --bibliography="$bib" -o "$pdf"
     }
   }
 
