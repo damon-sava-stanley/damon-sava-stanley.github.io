9d9cc2eca3a36f192b5da8c086dd1ff3c8292725
damon-sava-stanley <damonsava@gmail.com>
2018-04-27 14:28:21 -0400

    FIX Fixed up build script and reran
    Made it so that it now outputs to pages, which is a more sensible place.

--- blog.md
+++ blog.md
@@ -1,7 +1,7 @@
 ---
 title: Blog
 ...
-# [Natural Deduction Rendering](/pages/nd-render/index.html)
+# [Natural Deduction Rendering](/pages/nd-render)
 
 A simple web-app used for typesetting natural deduction proofs into
 SVG. The current version lives in [this repository.](https://github.com/damon-sava-stanley/nd-rendering)
--- build.ps1
+++ build.ps1
@@ -1,28 +1,18 @@
-param([switch] $tex)
+$tex=0;
 
-while ($true) {
-  Get-ChildItem "blogs/" -Filter *.md | Foreach-Object {
-    $bib="blogs/$_" -replace ".md", ".bib"
-    $html="blogs/html/$_" -replace ".md", ".html"
-    $pdf="blogs/html/$_" -replace ".md", ".pdf"
-    ./pp.exe -en -html "blogs/$_" | pandoc --template=template.html --toc --bibliography="$bib" -o "$html"
-    if ($tex) {
-      ./pp.exe -en -pdf "blogs/$_" | pandoc --toc --bibliography="$bib" -o "$pdf"
-    }
+Get-ChildItem "blogs/" -Filter *.md | Foreach-Object {
+  $dir="pages/$_" -replace ".md", ""
+  if(!(Test-Path -Path $dir)) {
+    mkdir $dir
   }
-
-  Get-ChildItem "slides/" -Filter *.md | Foreach-Object {
-    $bib="slides/$_" -replace ".md", ".bib"
-    $html="slides/html/$_" -replace ".md", ".html"
-    $pdf="slides/html/$_" -replace ".md", ".pdf"
-    ./pp.exe -en -html slides/$_ | pandoc -t revealjs -s -V revealjs-url=reveal --bibliography="$bib" -o "$html" --include-in-header=reveal.css.html
-    rm tmp.md
-    if ($tex) {
-      ./pp.exe -en -pdf slides/$_ | pandoc -t tmp.md --bibliography="$bib" -o "$pdf"
-    }
+  $html="$dir/index.html"
+  $pdf="$dir/index.pdf"
+  pp -en -html "blogs/$_" | pandoc --template=template.html --toc --filter pandoc-citeproc -o "$html"
+  if ($tex) {
+    pp -en -pdf "blogs/$_" | pandoc --toc --filter pandoc-citeproc -o "$pdf"
   }
-
-  rm -r tex2pdf.*/ 
-  pandoc blog.md --template=template.html --toc -o blog.html
-  sleep 30
 }
+
+rm -r tex2pdf.*/ 
+pandoc blog.md --template=template.html --toc -o blog.html
+pandoc index.md --template=template.html --toc -o index.html
